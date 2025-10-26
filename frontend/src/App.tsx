2e09c817ca2c4d2588e6eb6f3ebc8e09178e5595
Chris Anghel <chrisanghel04@gmail.com>
2025-10-26 11:11:52 -0400

    feat(frontend): add Ride Search

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,21 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import SignUpSuccess from "./pages/SignUpSuccess";
+import RidePostAndRequestPage from "./pages/RidePostAndRequestPage";
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+  <Route path="/" element={<RidePostAndRequestPage />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signup/success" element={<SignUpSuccess />} />
+        <Route path="/rides" element={<RidePostAndRequestPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
